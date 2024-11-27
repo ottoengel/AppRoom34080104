ecee91bef890272e3c25622851078d5c28acbe24
Otto <ottoengelhardt18@gmail.com>
2024-11-26 21:59:31 -0300

    configurações clonadas

---
+++ app/src/main/java/br/edu/up/rgm34080104/MainActivity.kt
@@ -0,0 +1,31 @@
+package br.edu.up.rgm34080104
+
+import InventoryApp
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import br.edu.up.rgm34183311.ui.theme.InventoryTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            InventoryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    InventoryApp()
+                }
+            }
+        }
+    }
+}
